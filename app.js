5a9e489c6bb2c134cdcc068fa68bc8ee2b1fcbe5
rehman8461 <stuvwxyzar336903@gmail.com>
2024-02-22 09:45:35 -0800

    rehman

---
+++ app.js
@@ -0,0 +1,30 @@
+// chapter 1
+alert("error! please enter a valid passworf");
+alert("welcome to js land \n happy coding!");
+alert("welcome to js land");
+alert("Happy coding! \n Prev;ent this page from creating additional dialoges.")
+alert("hello...i can run JShrough my web browsers console  ")
+
+// chapter 2
+
+var username;
+var myname = "abdul rehman";
+var meesage = "hello world";
+var age = "15";
+var biodata ="web developer";
+
+alert(myname);
+alert(meesage);
+alert(age);
+alert(biodata);
+
+
+var variable1 = "PIZZA \n PIZZ \n PIZ \n PI \n P"
+alert(variable1);
+
+
+var email = "abdulrehmanabc63@gmail.com";
+var final = "my email address is abdulrehmanabc63@gmail.com";
+alert(final);
+
+
